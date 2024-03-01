94293b2fa87ecdbbdfabfe66ea0ce09e38f3b99a
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2024-03-01 23:35:41 +0530

    updated vite config to implement manual chunking

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,22 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  preview: {
+    port: 4000,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
