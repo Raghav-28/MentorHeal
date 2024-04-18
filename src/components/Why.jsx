1afe7920eebba8597d62dd3da0cd3033bcf39c37
Raghav_28 <kraghavgupta@gmail.com>
2024-04-18 23:33:45 +0530

    Updated the style  On branch main     Changes to be committed:
    	modified:   src/components/NavBar.jsx
    	modified:   src/components/Steps.jsx
    	modified:   src/components/Tagline.jsx
    	modified:   src/components/Why.jsx

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -32,29 +32,29 @@ const Why = () => {
   ];
 
   return (
-    <div className="px-1.5 flex flex-col items-center justify-center w-[100vw] mx-auto  my-28 space-y-8 bg-gradient-to-l from-[#e9f3ee] to-[#f8ebe5] p-16">
+    <div className="px-1.5 flex flex-col items-center justify-center w-[100vw] mx-auto  my-28 space-y-8 bg-gradient-to-l from-[#dbf0e5] to-[#e9d0c5] p-16">
       <div>
-        <h1 className="text-3xl font-kanit md:text-4xl ">
+        <h1 className="text-4xl font-kanit md:text-4xl font-semibold">
           Why{" "}
           <span className="font-kanit text-transparent bg-gradient-to-l from-[#4a7999] via-[#5b99c3] to-[#4c8db8] bg-clip-text">
             MentorHeal?
           </span>
         </h1>
       </div>
-      <div className="flex flex-col items-center justify-center gap-5 lg:gap-10 lg:flex-row ">
+      <div className="flex flex-col items-center justify-center gap-7 lg:gap-10 lg:flex-row ">
         {Data.map((_, i) => {
           return (
             <React.Fragment key={i}>
-              <div className="flex flex-col items-center space-y-3">
+              <div className="flex flex-col items-center space-y-5 ">
                 <div>
                   <img
                     src={_.image}
                     alt=""
-                    className="w-24 h-24 lg:w-32 lg:h-32"
+                    className="rounded-full  w-36 h-36 border-4 border-black lg:w-40 lg:h-40 transition-transform duration-300 transform hover:scale-110  shadow-xl shadow-slate-500 hover:shadow-xl hover:shadow-black-500 "
                   />
                 </div>
                 <div>
-                  <h1 className="font-kanit">{_.title}</h1>
+                  <h1 className=" text-2xl font-kanit transition-transform duration-300 transform hover:scale-110">{_.title}</h1>
                 </div>
               </div>
             </React.Fragment>
